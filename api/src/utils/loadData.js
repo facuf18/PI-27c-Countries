fd22546364c902ff1a527e6b7adf9e853032a059
Facundo <facufaccioli18@gmail.com>
2022-08-04 10:28:34 -0300

    final v2

--- api/src/utils/loadData.js
+++ api/src/utils/loadData.js
@@ -25,9 +25,11 @@ const getCountriesFromApi = async () => {
 const setCountriesToDb = async () => {
   try {
     const countries = await getCountriesFromApi();
-    countries.map(async country => {
-      await Country.create(country);
-    });
+    await Promise.all(
+      countries.map(async country => {
+        await Country.create(country);
+      })
+    );
   } catch (err) {
     throw new Error(err);
   }
